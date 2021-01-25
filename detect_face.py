1121143adf535982f635b80d4be627a14cbe0220
Nickinfinity <nikhilg.me@gmail.com>
2021-01-25 22:00:08 +0530

    Python & OpenCV code for face detection

--- detect_face.py
+++ detect_face.py
@@ -1,6 +1,24 @@
 import cv2
 
-// load haarcascade_frontalface_default
+
+# load haarcascade_frontalface_default
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+# load test image
+img = cv2.imread('test.jpg')
 
+#opencv only detecs gray scale images
+gray= cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#detect faces
+faces = face_cascade.detectMultiScale(gray,1.8,2)
+
+# draw rectangle around image
+for(x,y,w,h) in faces:
+    cv2.rectangle(img, (x,y), (x+w, y+h),(255,0,0),2)
+
+# show image
+cv2.imshow('img',img)
+
+# wait for key press tp close the image window
+cv2.waitKey()
